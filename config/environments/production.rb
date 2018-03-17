de7c1d38179ce5f16bd224db25620c2945f295e7
brendanoneill2016 <oneill.brendan88@gmail.com>
2018-03-17 18:33:05 +0000

    Updated Sendgrid settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'floating-dawn-21615.herokuapp.com'
+  host = 'radiant-refuge-88003.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',

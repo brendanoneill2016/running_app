912982a5eee2b05e8709112217f995ca1e0a16e3
brendanoneill2016 <oneill.brendan88@gmail.com>
2018-03-17 19:11:01 +0000

    Second attempt

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,6 +1,6 @@
 class SignupsController < ApplicationController
 	before_action :set_event
-	skip_before_filter :verify_authenticity_token 
+	skip_before_action :verify_authenticity_token 
 	
 
 	def set_event
